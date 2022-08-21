aa27d8096d2223b1d528f4e5822e9b7a3744ffb7
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-08-21 21:06:03 +0600

    Create bayfiles.py

---
+++ src/bayfiles.py
@@ -0,0 +1,22 @@
+from requests import get
+from requests import post
+
+
+class BayFiles:
+	def __init__(self):
+		self.api = "https://api.bayfiles.com"
+		self.headers = {
+			"User-Agent": "Mozilla/5.0 (Android; U; ru-RU) AppleWebKit/533.19.4 (KHTML, like Gecko) AdobeAIR/33.0"
+		}
+		
+	def upload_file(self, file: str):
+		files = {"file": open(file, "rb")}
+		return post(
+			f"{self.api}/upload",
+			files=files,
+			headers=self.headers).json()
+		
+	def get_file_info(self, file_id: str):
+		return get(
+			f"{self.api}/v2/file/{file_id}/info",
+			headers=self.headers).json()
